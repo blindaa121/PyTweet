51909eafd78dc7d0650f85f016078e5ffb9e0eb2
Bryan <blindaa121@gmail.com>
2020-12-08 17:07:14 -0800

    Create tweet detail view

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Whats up</h1>")
+    # return HttpResponse("<h1>Whats up</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JS
+    Return JSON data
+    """
+    data = {
+        "id": tweet_id,
+        # "content": obj.content,
+        #"image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    
+    return JsonResponse(data, status=status)
